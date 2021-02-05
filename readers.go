841e5edd5a3b21507f34190303718ca2fda71e37
Tony <haitrieu.nguyen@foodycorp.vn>
2021-02-05 17:08:08 +0700

    readers

---
+++ readers.go
@@ -0,0 +1,17 @@
+// Exercise: Readers
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+func (r MyReader) Read(b []byte) (int, error) {
+	for i, _ := range b {
+		b[i] = 'A'
+	}
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
