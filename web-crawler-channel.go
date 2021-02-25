0644ab3312edd79dbf04e52cf0ff751352a8c80b
Tony <haitrieu.nguyen@foodycorp.vn>
2021-02-25 13:38:26 +0700

    solution using channel

--- web-crawler-channel.go
+++ web-crawler-channel.go
@@ -1,76 +1,60 @@
-// Exercise: Web Crawler using sync.Mutex
+// Exercise: Web Crawler using channel
 package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
-type Cache struct {
-	mu sync.Mutex
-	m  map[string]bool
+type Pair struct {
+	urls  []string
+	depth int
 }
 
-type Counter struct {
-	mu     sync.Mutex
-	number int
-}
-
-func (c *Counter) Add(v int) {
-	c.mu.Lock()
-	c.number += v
-	c.mu.Unlock()
-}
-
-func (c *Counter) Get() int {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	return c.number
-}
-
-func Crawl(url string, depth int, fetcher Fetcher) {
-	visited := Cache{m: make(map[string]bool)}
-	numWorker := Counter{number: 1}
-	crawl(url, depth, fetcher, &visited, &numWorker)
-	for numWorker.Get() > 0 {
-	}
-}
-
-func crawl(url string, depth int, fetcher Fetcher, visited *Cache, numWorker *Counter) {
-	defer numWorker.Add(-1)
-
-	visited.mu.Lock()
-	exists := visited.m[url]
-	visited.m[url] = true
-	visited.mu.Unlock()
-
-	if exists || depth <= 0 {
-		return
-	}
-
+func crawRoutine(url string, depth int, ch chan Pair, fetcher Fetcher) {
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
+		ch <- Pair{[]string{}, 0}
 		fmt.Println(err)
 		return
 	}
+
 	fmt.Printf("found: %s %q\n", url, body)
-
-	numWorker.Add(len(urls))
-	for _, u := range urls {
-		go crawl(u, depth-1, fetcher, visited, numWorker)
+	if depth < 1 {
+		ch <- Pair{[]string{}, 0}
+	} else {
+		ch <- Pair{urls, depth - 1}
 	}
+}
 
-	return
+func Crawl(url string, depth int, fetcher Fetcher) {
+	cache := make(map[string]bool)
+	ch := make(chan Pair, 10)
+	ch <- Pair{[]string{url}, depth}
+	n := 1
+	for p := range ch {
+		for _, u := range p.urls {
+			if !cache[u] {
+				cache[u] = true
+				n += 1
+				go crawRoutine(u, p.depth, ch, fetcher)
+			}
+		}
+		n -= 1
+		if n == 0 {
+			break
+		}
+	}
 }
 
 func main() {
 	Crawl("https://golang.org/", 4, fetcher)
 }
 
+// fakeFetcher is Fetcher that returns canned results.
 type fakeFetcher map[string]*fakeResult
 
 type fakeResult struct {
